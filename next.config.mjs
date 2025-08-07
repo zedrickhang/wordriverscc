166a208dcaf40bdf667239a0e8d6d0d98eb884e8
zed <104608989+zedrickhang@users.noreply.github.com>
2025-08-07 10:18:13 +0800

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa';
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
